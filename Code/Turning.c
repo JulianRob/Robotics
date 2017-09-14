b6e78fc31c23141cbecc8be03cad887d15f3d2f2
Julian Ventura <julian.ventura20@gmail.com>
2017-09-14 14:51:22 -0400

    Added forward and back function

--- Code/Turning.c
+++ Code/Turning.c
@@ -1,27 +1,48 @@
-#pragma config(Motor,  port1,           FRWheel,       tmotorVex393_HBridge, openLoop)
+#pragma config(Motor,  port1,           RWheel,       tmotorVex393_HBridge, openLoop)
+#pragma config(Motor,  port10,          LWheel,       tmotorVex393_HBridge, openLoop)
 
 //This turns the robot to the left.
 void turnLeft()
 {
-		motor[FRWheel] = 100; //127 is the highest speed.
+		motor[RWheel] = 100;
 		wait1Msec(1300);
 }
 
 //This turns the robot to the right.
 void turnRight()
 {
-		motor[FRWheel] = -100; //127 is the highest speed.
+		motor[RWheel] = -100;
 		wait1Msec(1300);
 }
 
+//The robot moves forward
+void moveForward()
+{
+	motor[RWheel] = 100;
+	motor[LWheel] = -100;
+	wait1Msec(400);
+}
+
+//The robot moves back
+void moveBack()
+{
+	motor[RWheel] = -100;
+	motor[LWheel] = 100;
+	wait1Msec(400);
+}
+
+/* NOTES
+****************************************
+//127 is the highest speed.
+//We should get a jyro for turning for more turning consistency.
+****************************************
+*/
 task main()
 {
 	int x = 0;
-	if(x == 0)
+	while(x == 0)
 	{
-		//We should get a jyro for turning for more consistency
-		turnLeft();
-		turnRight();
+		moveBack();
 		x+=1;
 	}
 
