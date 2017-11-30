f33b136268cb05ea693876c52b03f00eb7b06106
Julian Ventura <julian.ventura20@gmail.com>
2017-11-30 16:20:31 -0500

    no change

---
+++ Code/Autonomous.c
@@ -0,0 +1,15 @@
+#pragma config(Motor,  port2,           clawGrip,      tmotorVex393_MC29, openLoop) //Opens and close
+#pragma config(Motor,  port3,           RWheel,        tmotorVex393_MC29, openLoop, reversed) //Right Wheel
+#pragma config(Motor,  port5,           LWheel,        tmotorVex393_MC29, openLoop, reversed) //Left Wheel
+#pragma config(Motor,  port6,           scissorLift1,  tmotorVex393_MC29, openLoop) //One scissor lift
+#pragma config(Motor,  port7,           scissorLift2,  tmotorVex393_MC29, openLoop, reversed) //The other scissor lift.
+#pragma config(Motor,  port8,           clawLift,      tmotorVex393_MC29, openLoop) // Lifts up the claw.
+#pragma config(Motor,  port9,           FLift,         tmotorVex393_MC29, openLoop, reversed) //Fork Lift
+
+task main()
+{
+	//Move forward
+	//Raise the Claw
+	//Drop the claw
+
+}
