560ed536042c0fd1db1bbaa33ab7676ad52bea08
Julian Ventura <julian.ventura20@gmail.com>
2018-02-08 15:43:00 -0500

    Organized Folders

--- Code/Current/CompetitionTemplate.c
+++ Code/Current/CompetitionTemplate.c
@@ -64,135 +64,79 @@ void pre_auton()
 
 task autonomous()
 {
- 	int test = -100;
-  float limit = -2; //-2
+ 	int run = true;
 
-	motor[RWheel] = 0;
-	motor[LWheel] = 0;
-
-	while (limit == test)
+	while(run == true)
 	{
-		//motor[scissorLift] = -65; //lifts scissor lift up
-  	motor[FLift] = 100; //Down
-		//motor[clawLift] = 50; //arm descends
-		//motor[clawGrip] = 50; //claw grip opens
-	  //motor[RFWheel] = 100; //moves forward
-		//motor[RBWheel] = 100; //moves forward
-		//motor[LFWheel] = 50; //moves forward
-	}
-
-	while(limit == -2)
-	{
-		motor[clawLift] = -60; //arm lifts
-		motor[clawGrip] = -17; //grips closes
-		wait1Msec(500);
-		limit = -1;
-	}
-
-	while(limit == -1)
-	{
-		motor[clawLift] = 65; //arm lowers
-		motor[clawGrip] = -10; //grip closes
+		motor[clawGrip]	= -20; //closes
+		motor[clawLift] = 50; //lowers the arm
 		wait1Msec(1000);
-		limit = -0.5;
-	}
 
-	while(limit == -0.5)
-	{
-			motor[clawGrip] = -50;
-			wait1Msec(500);
-			limit = -0.2;
-	}
+		motor[clawGrip]	= -50; //closes
+		motor[clawLift] = -80; //lifts the arm
+		motor[RScissorLift] = 100; //lifts scissor lift
+		motor[LScissorLift] = 100; //lifts scissor lift
+		wait1Msec(2500);
 
-	while(limit == -0.2)
-	{
-		motor[clawLift] = -50;
-		wait1Msec(500);
-		limit = 0;
-	}
-
-	while(limit == 0) //This turns right.
-	{
-	 motor[clawGrip] = -50;
-	 motor[clawLift] = 0;
-	 limit = 1;
-	}
-
-	while(limit == 1)
-	{
-		motor[RWheel] = 80;
-		motor[LWheel] = 80;
-
-		motor[LScissorLift] = 0;
 		motor[RScissorLift] = 0;
-
-	 	motor[clawLift] = -90;
-		wait1Msec(3300); //3046
-
-		motor[RWheel] = -25;
-  	motor[LWheel] = -25;
-  	wait1Msec(500);
-
-		motor[clawLift] = 0;
-		motor[clawGrip] = 0;
-
-		motor[RWheel] = 0;
-  	motor[LWheel] = 0;
-		limit = 2.1;
-	}
-
-	while(limit == 2.1)
-	{
-		motor[clawGrip] = 45;
-		wait1Msec(500);
-
-		motor[clawGrip] = 0;
-
-		motor[LScissorLift] = -100;
-		motor[RScissorLift] = -100;
-
+		motor[LScissorLift] = 0;
+		motor[RWheel] = 40;
+		motor[LWheel] = 40;
 		wait1Msec(2000);
-
-		motor[LScissorLift] = 0;
-		motor[RScissorLift] = 0;
-
-		motor[RWheel] = 50;
-		motor[LWheel] = 50;
-		wait1Msec(500);
 
 		motor[RWheel] = 0;
 		motor[LWheel] = 0;
-		motor[FLift] = -100;
-		wait1Msec(1000); //Change time for lowering (used to be 3000)
+		motor[clawLift] = 50; //lowers the arm
+		wait1Msec(1000);
 
-		motor[FLift] = 0;
-		motor[RWheel] = -80;
-   	motor[LWheel] = -80;
-		wait1Msec(2000);
+		motor[clawGrip]	= 60; //opens
+		motor[clawLift] = 0;
+		wait1Msec(400);
 
-		motor[RWheel] = 80;
-  	motor[LWheel] = -10;
-  	wait1Msec(3000);
-
-  	motor[RWheel] = 100;
-  	motor[LWheel] = 100;
-		wait1Msec(2000);
+		motor[clawGrip] = 0;
+		motor[RWheel] = -50;
+		motor[LWheel] = -50;
+		wait1Msec(1000);
 
 		motor[RWheel] = 0;
-  	motor[LWheel] = 0;
-  	motor[FLift] = 100;
-  	wait1Msec(1000); //Change time for lowering (used to be 3000)
+		motor[LWheel] = 0;
+		motor[clawGrip]	= -30;
+		wait1Msec(1000);
 
-  	motor[FLift] = 0;
-  	motor[RWheel] = -50;
-  	motor[LWheel] = -50;
-  	wait1Msec(2000);
+		motor[LScissorLift] = -100;
+		motor[RScissorLift] = -100;
+		motor[clawLift] = 50;
+		wait1Msec(2500);
+
+		motor[LScissorLift] = 0;
+		motor[RScissorLift] = 0;
+		motor[clawLift] = 0;
+		motor[clawGrip]	= 80;
+		wait1Msec(500);
+
+		motor[clawGrip]	= 0;
+		motor[RWheel] = 50;
+		motor[LWheel] = 0;
+		wait1Msec(2000);
+
+		motor[RWheel] = 50;
+		motor[LWheel] = 50;
+		wait1Msec(1500);
 
 		motor[RWheel] = 0;
-  	motor[LWheel] = 0;
+		motor[LWheel] = 0;
+		motor[clawGrip]	= -80;
+		wait1Msec(700);
 
-		limit = 2.2;
-  }
+		motor[clawGrip]	= 0;
+
+		run = false;
+	}
+
+	while(run == false)
+	{
+
+	}
 }
 
 /*---------------------------------------------------------------------------*/
